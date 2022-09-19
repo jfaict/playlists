e8b6809333a1089a506e962cb0679ac9f0fd8be1
Jonas Faict <jfaict@omp.com>
2022-09-19 19:41:04 +0200

    create protected route handler

---
+++ lib/auth.ts
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+// handler is a function take takes a request and response and does something.
+// function below takes a handler as input, but first checks credentials before executing the handler > to protect our routes
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const { TRAX_ACCESS_TOKEN: token } = req.cookies;
+
+    if (token) {
+      let user;
+
+      // wrap in try catch to catch case where token is valid (and does not contain a user)
+      try {
+        const { id } = jwt.verify(token, "hello");
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+
+        if (!user) {
+          throw new Error("Not real user");
+        }
+      } catch (error) {
+        res.status(401);
+        res.json({ error: "Not authorized" });
+        return;
+      }
+      return handler(req, res, user);
+    }
+
+    res.status(401);
+    res.json({ error: "Not authorized" });
+  };
+};
