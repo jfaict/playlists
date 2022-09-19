cb62eaec954981c257b3a1e2dca6f5209b4d3098
Jonas Faict <jfaict@omp.com>
2022-09-19 22:22:44 +0200

    fix middleware issue

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,16 @@
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 const signedinPages = ["/", "/playlist", "/library"];
 
-export default function middleware(req) {
+export default function middleware(req: NextRequest) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-    const token = req.cookies.TRAX_ACCESS_TOKEN;
+    const token = req.cookies.get("TRAX_ACCESS_TOKEN");
 
     if (!token) {
       const url = req.nextUrl.clone();
       url.pathname = "/signin";
-      return NextResponse.rewrite(url);
+      return NextResponse.redirect(url);
     }
   }
 }
