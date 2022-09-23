cbb7445ac7f61999610ac3e1b62cf4e2ba981222
Jonas Faict <jfaict@omp.com>
2022-09-23 07:51:34 +0200

    add playlist page header

--- lib/auth.ts
+++ lib/auth.ts
@@ -33,3 +33,8 @@ export const validateRoute = (handler) => {
     res.json({ error: "Not authorized" });
   };
 };
+
+export const validateToken = (token) => {
+  const user = jwt.verify(token, "hello");
+  return user;
+};
